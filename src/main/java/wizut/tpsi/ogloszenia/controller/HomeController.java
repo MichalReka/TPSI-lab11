96690ee852b78a652b7ead02490710be2bcb0446
Michał <michajoba@gmail.com>
2020-05-16 19:56:40 +0200

    upload lab 11

--- src/main/java/wizut/tpsi/ogloszenia/controller/HomeController.java
+++ src/main/java/wizut/tpsi/ogloszenia/controller/HomeController.java
@@ -3,13 +3,15 @@ package wizut.tpsi.ogloszenia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
-import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.*;
 import wizut.tpsi.ogloszenia.services.OffersService;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,10 +19,21 @@ public class HomeController {
     private OffersService offersService;
     @RequestMapping("/")
     public String home(Model model) throws SQLException {
-        CarModel carModel = offersService.getModel(3);
-        CarManufacturer carManufacturer = offersService.getCarManufacturer(2);
-        model.addAttribute("carModel", carModel);
-        model.addAttribute("carManufacturer",carManufacturer);
-        return "index";
+        List<CarManufacturer> carManufacturers = offersService.getCarManufacturers();
+        List<CarModel> carModels = offersService.getCarModels();
+
+        List<Offer> offers = offersService.getOffers();
+
+        model.addAttribute("carManufacturers", carManufacturers);
+        model.addAttribute("carModels", carModels);
+        model.addAttribute("offers", offers);
+
+        return "offersList";
+    }
+    @GetMapping("/offer/{id}")
+    public String offerDetails(Model model, @PathVariable("id") Integer id) {
+        Offer offer = offersService.getOffer(id);
+        model.addAttribute("offer", offer);
+        return "offerDetails";
     }
 }
